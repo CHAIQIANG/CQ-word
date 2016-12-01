7be19ecb1753850016f1192427f6a86058376103
CHAIQIANG <2843317807@qq.com>
2016-12-01 10:35:45 +0800

    zhineghua

---
+++ sz.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl -w
+$num = 1234;
+$str = '12ab';
+print $num, " ",$str,"\n";
+$num_or_str = $num+$str;
+print $num_or_str,"\n";
+$num_or_str = $num.$str;
+print $num_or_str,"\n";
+exit;
