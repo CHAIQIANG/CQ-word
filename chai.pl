69130051001945e53d742f1a52e15cf9ba3c3475
CHAIQIANG <2843317807@qq.com>
2016-12-01 08:40:20 +0800

    lianxi

---
+++ chai.pl
@@ -0,0 +1,15 @@
+#/usr/bin/perl -w
+$word = 'MNIDDKL';
+if($word eq 'WJAISJDI'){
+print "WJAISJDI\n";
+}
+elsif($word eq 'DDAAHE'){
+print "DDAAHE\n";
+}
+elsif($word eq 'MNIDDKL'){
+print "MNIDDKL-magic word!\n";
+}
+else{
+print "\"$word\" is not sure.\n";
+}
+exit;
