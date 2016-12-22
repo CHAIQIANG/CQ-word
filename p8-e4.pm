f34d7e736efb546a24a1658f258487d0aab5d73a
linux <2843317807@qq.com>
2016-12-22 10:11:00 +0800

    print_sequence

---
+++ p8-e4.pm
@@ -0,0 +1,9 @@
+#print_sequence geshihua
+sub print_sequence{
+my($sequence,$length) = @_;
+use strict;
+use warnings;
+for(my $pos = 0;$pos < length($sequence);$pos += $length){
+print substr($sequence,$pos,$length),"\n";
+}
+}
